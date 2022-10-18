e7a867a2262b59ccf8fd7611f63198429edde17a
Shigeru Owada <owd@hoikutech.com>
2022-10-18 21:18:28 +0900

    New data added

--- calibrate-estimate.js
+++ calibrate-estimate.js
@@ -2,7 +2,7 @@
 // Using least square fitting
 // https://qiita.com/quzq/items/c1a4929f47d986b0f77f
 
-const A1 = [ // x:gram,  y:value
+const A1 = [ // y:gram,  x:value
 	{y:474,x:235000},
 	{y:426,x:208030},
 	{y:356,x:175050},
@@ -12,8 +12,19 @@ const A1 = [ // x:gram,  y:value
 	{y:32,x:22900},
 	//{y:0,x:880},
 	{y:0,x:1176},
+
+
+	{y:1357.1, x:666058.5},
+	{y:751.2, x:367988.4},
+	{y:1936.7, x:947670.1},
+	{y:1005.5, x:492506.9},
+	{y:1551.6, x:759360.6},
+	{y:2788.6, x:1366549.6},
+	{y:1649.0, x:807073.7},
+	{y:1357.1, x:664450}
+
 ].reverse();
-const B1 = [ // x:gram,  y:value
+const B1 = [ // y:gram,  x:value
 	{y:474,x:54650},
 	{y:426,x:48630},
 	{y:356,x:40820},
@@ -23,8 +34,19 @@ const B1 = [ // x:gram,  y:value
 	{y:32,x:5060},
 	//{y:0,x:-10},
 	{y:0,x:15},
+
+
+	{y:1357.1, x:156257.5},
+	{y:1649, x:190363.5},
+	{y:2782.4, x:322056.9},
+	{y:1551.8, x:178002.5},
+	{y:1005.8, x:115543.8},
+	{y:761.1, x:87862.9},
+	{y:1927.1, x:222029.7},
+
 ].reverse();
-const A2 = [ // x:gram,  y:value
+
+const A2 = [ // y:gram,  x:value
 	{y:474,x:210850},
 	{y:426,x:188000},
 	{y:356,x:157160},
@@ -34,8 +56,17 @@ const A2 = [ // x:gram,  y:value
 	{y:32,x:17365},
 	//{y:0,x:-450},
 	{y:0,x:-955},
+
+
+	{y:1927.1, x:857288.8},
+	{y:760.6, x:339558},
+	{y:1005.8, x:447046.1},
+	{y:1551.4, x:688513.8},
+	{y:2788.9, x:1235274.6},
+	{y:1649.5, x:731839.1},
+	{y:1357, x:603110.9},
 ].reverse();
-const B2 = [ // x:gram,  y:value
+const B2 = [ // y:gram,  x:value
 	{y:474,x:54890},
 	{y:426,x:49225},
 	{y:356,x:40930},
@@ -45,6 +76,15 @@ const B2 = [ // x:gram,  y:value
 	{y:32,x:4725},
 	//{y:0,x:70},
 	{y:0,x:167},
+
+
+	{y:1927.3, x:221289.5},
+	{y:754.1, x:86601.7},
+	{y:1005.9 ,x:115404.3},
+	//{y:1261.7, x:178372.4},
+	//{y:2644.2, x:319935.7},
+	{y:1652.5, x:192652.3},
+	{y:1357.1, x:156220.8},
 ].reverse();
 
 
@@ -70,17 +110,23 @@ function estimate(x,params){
 
 
 const names = ['A1','B1','A2','B2'];
+let errs = [];
+
 [A1,B1,A2,B2].forEach((dt,i)=>{
 	const params = lsm(dt);
 	console.log(`${names[i]}: (a,b)=(${params.a},${params.b})`);
+	let e = 0;
 	dt.forEach(d=>{
-		console.log(`Correct:${d.y}g , Estimated: ${Math.round(estimate(d.x,params))}g`);
+		const er = Math.pow(d.y-estimate(d.x,params),2);
+		console.log(`Correct:${d.y}g , Estimated: ${Math.round(estimate(d.x,params))}g (err=${er})`);
+		e += er;
 	});
+	errs.push(e);
 });
 
 // Python output
 [A1,B1,A2,B2].forEach((dt,i)=>{
 	const params = lsm(dt);
-	console.log(`${names[i]}p = [${params.a},${params.b}]`);
+	console.log(`${names[i]}p = [${params.a},${params.b}] , err: ${errs[i]}`);
 });
 
